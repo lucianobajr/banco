d7320b85052e58fa665e62d1e0571f70b68c91cb
Luciano Belo <luciano.alcantara@ufv.br>
2021-04-12 10:39:31 -0300

    Update 2-g.sql

--- lista-07/scripts/2-g.sql
+++ lista-07/scripts/2-g.sql
@@ -3,3 +3,9 @@ SELECT departamento.nomedepto,AVG(salario) AS media_salario
     INNER JOIN departamento ON funcionario.did=departamento.did 
     GROUP BY departamento.did 
     ORDER BY departamento.nomedepto; 
+
+SELECT departamento.nomedepto,AVG(salario) AS media_salario 
+	FROM funcionario 
+	NATURAL JOIN departamento
+	GROUP BY departamento.nomedepto 
+	ORDER BY departamento.nomedepto; 
