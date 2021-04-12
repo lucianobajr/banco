8207b9855f214bb400da06582ebdffa07a12638f
Luciano Belo <luciano.alcantara@ufv.br>
2021-04-12 10:58:16 -0300

    Update 3-c.sql

--- lista-07/scripts/3-c.sql
+++ lista-07/scripts/3-c.sql
@@ -2,3 +2,9 @@ SELECT paciente.NM_PAC,atendimento.DIA_ATEND
 	FROM paciente 
     INNER JOIN atendimento ON paciente.CD_PAC=atendimento.CD_PAC 
     HAVING atendimento.DIA_ATEND="1996-04-09";
+    
+    
+    SELECT paciente.NM_PAC
+	FROM paciente 
+    	NATURAL JOIN atendimento 
+    	WHERE atendimento.DIA_ATEND="1996-04-09";
