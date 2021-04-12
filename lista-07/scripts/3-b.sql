5c4dd6b53d0158e8b1a3a713dc3071e6f8c11819
Luciano Belo <luciano.alcantara@ufv.br>
2021-04-12 10:55:43 -0300

    Update 3-b.sql

--- lista-07/scripts/3-b.sql
+++ lista-07/scripts/3-b.sql
@@ -2,3 +2,8 @@ SELECT paciente.*,convenio.DESC_CONVENIO
 	FROM paciente 
 	INNER JOIN convenio ON paciente.CONV_PAC=convenio.COD_CONVENIO 
     HAVING convenio.DESC_CONVENIO="Unimed - código 11347";
+    
+SELECT paciente.*,convenio.DESC_CONVENIO 
+	FROM paciente 
+	JOIN convenio ON paciente.CONV_PAC=convenio.COD_CONVENIO 
+    WHERE convenio.DESC_CONVENIO="Unimed - código 11347";
