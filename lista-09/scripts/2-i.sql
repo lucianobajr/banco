7827bb5b0291ff63d3915f77ca11966f0c7e3c18
Luciano Belo <luciano.alcantara@ufv.br>
2021-04-26 10:42:58 -0300

    Update 2-i.sql

--- lista-09/scripts/2-i.sql
+++ lista-09/scripts/2-i.sql
@@ -1,3 +1,7 @@
 SELECT montadora,modelo,
 	ROUND(RAND(1) * (10.00 - 0.00),2) + 0 AS NOTA 
 	FROM carros ORDER BY NOTA;
+	
+SELECT montadora,modelo,
+	ROUND(RAND(1) * 10.00,2) + 0 AS NOTA 
+	FROM carros ORDER BY NOTA;
