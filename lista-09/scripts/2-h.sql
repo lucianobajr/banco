c5530fc78a2fd4f712fb7652acc6eefec1520de0
Luciano Belo <luciano.alcantara@ufv.br>
2021-04-26 10:38:26 -0300

    Update 2-h.sql

--- lista-09/scripts/2-h.sql
+++ lista-09/scripts/2-h.sql
@@ -1,3 +1,7 @@
 SELECT montadora,modelo,
 	FLOOR(ROUND(RAND() * (10.00 - 0.00),2) + 0) AS nota 
 	FROM carros ORDER BY nota;
+	
+SELECT montadora,modelo,
+	FLOOR(RAND() * (10.00)) AS nota 
+	FROM carros ORDER BY nota;
