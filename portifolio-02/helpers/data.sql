96a39efae403e7f277090f49f73e1b74cfaa19a4
lucianobajr <luciano.alcantara@ufv.br>
2021-05-03 21:02:06 -0300

    change  SCHEMA

---
+++ portifolio-02/helpers/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO `movie` (
+    `movie_id`, 
+    `movie_name`, 
+    `production_year`,
+    `votes`,
+    `ranking`,
+    `rating`
+) 
+VALUES (1, 'Filme1', 2016,20,2,10),
+VALUES (2, 'Filme2', 2020,10,1,10),
+VALUES (3, 'Filme3', 2011,10,3,10),
+VALUES (4, 'Filme4', 2014,11,1,10);
