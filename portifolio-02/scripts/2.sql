55ceec7b284d1314f4b13b92d64f1c919a032b07
Luciano Belo <luciano.alcantara@ufv.br>
2021-05-19 22:17:19 -0300

    Update 2.sql

--- portifolio-02/scripts/2.sql
+++ portifolio-02/scripts/2.sql
@@ -2,5 +2,5 @@ SELECT genre_name,COUNT(*) AS quantidade_filmes
 	FROM movie
     NATURAL JOIN  movie_info
     NATURAL JOIN movie_genre
-    group by genre_name
+    GROUP BY genre_name
     ORDER BY COUNT(*);
